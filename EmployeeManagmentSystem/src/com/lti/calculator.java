13bd160f60d526ffffab6e8faf00f0e7aae0d6cb
shruti upreti <shrutiupreti15@gmail.com>
2020-08-06 09:46:07 +0530

    added dif

--- EmployeeManagmentSystem/src/com/lti/calculator.java
+++ EmployeeManagmentSystem/src/com/lti/calculator.java
@@ -6,9 +6,15 @@ public class calculator {
 	    Sum = a+b;
 	    return Sum;
 	  }
+	public static int sub(int a, int b){
+		int dif =0;
+	    dif = a-b;
+	    return dif;
+	  }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print(add(3,5));
+		System.out.print(sub(30,5));
 	}
 
 }

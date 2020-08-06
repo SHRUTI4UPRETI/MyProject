97caa660e4e79c4892e1d3c6d2b618e85071445f
shruti upreti <shrutiupreti15@gmail.com>
2020-08-06 09:28:07 +0530

    comitted

---
+++ EmployeeManagmentSystem/src/com/lti/calculator.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class calculator {
+	public static int add(int a, int b){
+		int Sum =0;
+	    Sum = a+b;
+	    return Sum;
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(add(3,5));
+	}
+
+}
